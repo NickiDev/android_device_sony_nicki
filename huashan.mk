9d74aea763c9de9c7098846fe8002afc43c8170b
Adam Farden <adam@farden.cz>
2014-06-27 04:27:48 +0200

    Don't try to build Android things

--- huashan.mk
+++ huashan.mk
@@ -32,7 +32,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
-    packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml
 
 # This device is xhdpi.  However the platform doesn't
@@ -223,7 +222,6 @@ PRODUCT_PACKAGES += \
 
 # Miscellaneous
 PRODUCT_PACKAGES += \
-    librs_jni \
     com.android.future.usb.accessory
 
 # Live Wallpapers
