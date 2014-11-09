523b5426c81df63bb9eacb4464dc900eb4e39e29
Arnav Gosain <gosain.arnav@gmail.com>
2014-11-09 03:57:03 -0500

    nicki: comment out kernel source/config

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -47,6 +47,8 @@ BOARD_KERNEL_CMDLINE := panic=3 console=ttyHSL0,115200,n8 androidboot.hardware=q
 BOARD_KERNEL_BASE := 0x80200000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
+TARGET_KERNEL_SOURCE := kernel
+TARGET_KERNEL_CONFIG := cyanogenmod_nicki_defconfig
 
 # Partitions
 TARGET_USERIMAGES_USE_EXT4 := true
